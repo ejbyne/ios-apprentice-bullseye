b9e4610a8d479beefd0dd9f5a3db3a02dea609aa
Edward Byne <ejbyne@gmail.com>
2015-05-12 16:43:35 +0200

    Adds code to records score and round and connects to interface

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -10,10 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
   
-  var currentValue: Int = 0
-  var targetValue: Int = 0
+  var currentValue = 0
+  var targetValue = 0
+  var score = 0
+  var round = 0
   @IBOutlet weak var slider: UISlider!
   @IBOutlet weak var targetLabel: UILabel!
+  @IBOutlet weak var scoreLabel: UILabel!
+  @IBOutlet weak var roundLabel: UILabel!
 
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -27,8 +31,10 @@ class ViewController: UIViewController {
   }
   
   @IBAction func showAlert() {
-    let message = "The value of the slider is: \(currentValue)"
-                  + "\nThe target value is: \(targetValue)"
+    let difference = abs(targetValue - currentValue)
+    let points = 100 - difference
+    score += points
+    let message = "You scored \(points) points"
     let alert = UIAlertController(title: "Hello, World",
                                   message: message,
                                   preferredStyle: .Alert)
@@ -45,6 +51,7 @@ class ViewController: UIViewController {
   }
   
   func startNewRound() {
+    round += 1
     targetValue = 1 + Int(arc4random_uniform(100))
     currentValue = 50
     slider.value = Float(currentValue)
@@ -52,6 +59,8 @@ class ViewController: UIViewController {
   
   func updateLabels() {
     targetLabel.text = String(targetValue)
+    scoreLabel.text = String(score)
+    roundLabel.text = String(round)
   }
 
 }
