85e8daf482b5094dd93e10abe97cf0db9c8b10b2
Edward Byne <ejbyne@gmail.com>
2015-05-15 16:20:44 +0200

    Adds readme file

--- Bullseye/ViewController.swift
+++ Bullseye/ViewController.swift
@@ -32,10 +32,22 @@ class ViewController: UIViewController {
   
   @IBAction func showAlert() {
     let difference = abs(targetValue - currentValue)
-    let points = 100 - difference
+    var points = 100 - difference
+    var title: String
+    if difference == 0 {
+      title = "Perfect!"
+      points += 100
+    } else if difference < 5 {
+      title = "You almost had it!"
+      points += 50
+    } else if difference < 10 {
+      title = "Pretty good!"
+    } else {
+      title = "Not even close..."
+    }
     score += points
     let message = "You scored \(points) points"
-    let alert = UIAlertController(title: "Hello, World",
+    let alert = UIAlertController(title: title,
                                   message: message,
                                   preferredStyle: .Alert)
     let action = UIAlertAction(title: "OK", style: .Default,
---
+++ README.md
@@ -0,0 +1,7 @@
+# Bullseye
+
+## Summary
+
+This is Tutorial 1 contained in the [iOS Apprentice book](http://www.raywenderlich.com/store/ios-apprentice) by Matthijs Hollemans.
+
+It was a really useful tutorial for clarifying the MVC structure of iOS apps and how everything fits together.
